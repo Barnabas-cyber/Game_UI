1a2d6e4366a7bc4f1858452dd78711def5f25d42
Barnbas-cyber <moloibarnabas@gmail.com>
2022-09-09 10:53:28 +0200

    Updates

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 // js script
 //score counting variables
-let playerScore = 0, computerScore = 0;
+let playerScore = 0, computerScore = 0, moves = 0;
+
 // function to decide the winner 
 function playerRound(playerSelection, computerSelection) {
     //empty string
     let results = '';
     //rock or paper 
     if (playerSelection.toUpperCase() == computerSelection) {
-        results = `It is a tie!`
+        results.textContent = `It is a tie!`
     }
     else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'paper') {
-        results = `You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}}`
+        results.textContent = `You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}}`
         computerScore++;
     } else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection == 'rock') {
-        results = `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        results.textContent = `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
         playerScore++;
     }
     //paper or scissors
@@ -21,20 +22,21 @@ function playerRound(playerSelection, computerSelection) {
         results = `You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
         computerScore++;
     } else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'paper') {
-        results = `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        results.textContent = `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
         playerScore++;
     }
     // rock or scissors
     if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'rock') {
         results = `You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
         computerScore++;
-    } else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'sccissors') {
-        results = `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+    } else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'scissors') {
+        results.textContent = `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
         playerScore++;
     }
 
     return results;
 }
+
 
 //calling the game function
 // game()
